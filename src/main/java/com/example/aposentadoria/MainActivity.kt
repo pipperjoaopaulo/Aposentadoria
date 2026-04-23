bb8b34edf51d36b8888bee5e68e2e6591800f0fb
PipperjoaopauloINFO5 <pipperjoaopaulo@gmail.com>
2026-04-23 12:33:42 -0300

    inicail

---
+++ src/main/java/com/example/aposentadoria/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.aposentadoria
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.aposentadoria.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val Items = listOf("masculino" , "feminina")
+
+
+    }
+}
