b11096a35641a9ea70880fc0143b9742bf3f8128
PipperjoaopauloINFO5 <pipperjoaopaulo@gmail.com>
2026-04-24 09:55:43 -0300

    aparencia feita falta a parte do codigo

--- src/main/java/com/example/aposentadoria/MainActivity.kt
+++ src/main/java/com/example/aposentadoria/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.aposentadoria
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,11 +10,20 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.aposentadoria.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val Items = listOf("masculino" , "feminina")
+        val items = listOf("MASCULINO" , "FEMININA")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, items)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spinner.adapter = adapter
+        //if ()
+        binding.botao.setOnClickListener {
+            val idade = binding.edittexto.text.toString().toLong()
+        }
+
 
 
     }
