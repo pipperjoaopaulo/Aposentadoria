20e73bf5bd1ae5f288949aef80b5e7e38f49fea8
PipperjoaopauloINFO5 <pipperjoaopaulo@gmail.com>
2026-04-30 12:49:45 -0300

    dificuldadesss

--- src/main/java/com/example/aposentadoria/MainActivity.kt
+++ src/main/java/com/example/aposentadoria/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.aposentadoria
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -15,16 +16,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val items = listOf("MASCULINO" , "FEMININA")
+        val items = listOf("MASCULINO", "FEMININA")
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, items)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         binding.spinner.adapter = adapter
         //if ()
         binding.botao.setOnClickListener {
             val idade = binding.edittexto.text.toString().toLong()
+            val sexo = binding.spinner.adapter.toString()
+            when{ sexo == "MASCULINO" ->{
+                var resultado = 65L - idade
+            }
+            sexo == "FEMININA" -> {
+                var resultado = 62 - idade
+            }
+            else ->{
+                "abc"
+            }
+            }
+            if (resultado > 0L) {
+                Log.d("Estimativa de Aposentadoria:", "${resultado}")
+            } else if (resultado < 0L) {
+                Log.d("Você já deveria estar aposentado:", "${resultado}")
+            } else {
+                Log.d("Hora da Aposentadoria:", "${resultado}")
+            }
+
+
         }
-
-
-
     }
 }
