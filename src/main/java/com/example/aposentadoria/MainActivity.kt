2b32f0c0042d308b15fc8d0b09211a36da84f8d2
PipperjoaopauloINFO5 <pipperjoaopaulo@gmail.com>
2026-05-07 12:51:09 -0300

    CONSEGUI!!!

--- src/main/java/com/example/aposentadoria/MainActivity.kt
+++ src/main/java/com/example/aposentadoria/MainActivity.kt
@@ -1,9 +1,8 @@
 package com.example.aposentadoria
-
+import android.R
 import android.os.Bundle
 import android.util.Log
 import android.widget.ArrayAdapter
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,32 +16,31 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val items = listOf("MASCULINO", "FEMININA")
-        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, items)
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        val adapter = ArrayAdapter(this, R.layout.simple_spinner_item, items)
+        adapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item)
         binding.spinner.adapter = adapter
         //if ()
         binding.botao.setOnClickListener {
-            val idade = binding.edittexto.text.toString().toLong()
-            val sexo = binding.spinner.adapter.toString()
-            when{ sexo == "MASCULINO" ->{
-                var resultado = 65L - idade
-            }
-            sexo == "FEMININA" -> {
-                var resultado = 62 - idade
-            }
-            else ->{
-                "abc"
-            }
-            }
-            if (resultado > 0L) {
-                Log.d("Estimativa de Aposentadoria:", "${resultado}")
-            } else if (resultado < 0L) {
-                Log.d("Você já deveria estar aposentado:", "${resultado}")
-            } else {
-                Log.d("Hora da Aposentadoria:", "${resultado}")
-            }
+            val sexo = binding.spinner.selectedItem as String
+            val idade = binding.edittexto.text.toString().toLongOrNull()
+            var resultado: Long
+            if (idade != null) {
+                if (sexo.trim() == "MASCULINO") {
+                    resultado = 65 - idade
+                } else {
+                    resultado = 62 - idade
+                }
+                binding.resposta.text = "Os Anos Restantes Para Você Se Aposentar São: $resultado"
+                if (resultado <= 0){
+                    binding.muitovelho.text = "Você já deveria estar APOSENTADO."
+                } else{
+                    binding.muitovelho.text = ""
+                }
+            }else{
+                binding.resposta.text ="informe uma idade valida."
 
-
+            }
         }
+
     }
-}
+    }
